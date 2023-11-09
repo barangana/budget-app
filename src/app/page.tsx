87076f69f2b87ad7f89af0d6b0a957652482e025
Adriel <baranganadriel@gmail.com>
2023-11-08 22:06:36 -0500

    chore: fix small typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
           'mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'
         }
       >
-        <h1 className=''>buydgeto</h1>
+        <h1 className=''>budgeto</h1>
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
           welcomes you
         </p>
