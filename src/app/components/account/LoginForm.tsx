de43fb3ce85452252c615a46e16946565200a03a
Adriel <baranganadriel@gmail.com>
2023-12-13 22:40:34 -0500

    feat: styled the login form

--- src/app/components/account/LoginForm.tsx
+++ src/app/components/account/LoginForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 // login takes in a username and password (temporarily)
 // todo: add google provider and other providers
@@ -11,24 +12,22 @@ interface LoginInfo {
 
 const LoginForm = () => {
   return (
-    <div>
-      <div className='grid place-items-center h-screen'>
-        <div className='border-solid border-2 border-black p-24'>
-          <form className=''>
-            <div className='flex flex-col items-center'>
-              <header className='pb-8 font-normal text-4xl'>budgeto</header>
-              <label>username</label>
-
-              <label>password</label>
-
-              <p>message</p>
-              <div className='flex justify-between w-44 pt-6 pb-12'>
-                <button>login</button>
-                <button>register</button>
-              </div>
-            </div>
-          </form>
-        </div>
+    <div className='grid place-items-center h-screen'>
+      <div className='border-solid border-2 border-black p-24 rounded-lg'>
+        <form>
+          <div className='flex flex-col items-center'>
+            <header className='pb-8 font-normal text-4xl'>Log In</header>
+            <input
+              className='rounded-lg bg-slate-200 px-12 py-2 my-4'
+              placeholder='username'
+            />
+            <input
+              className='rounded-lg bg-slate-200 px-12 py-2'
+              placeholder='password'
+            />
+            <Button className='container mt-4'>login</Button>
+          </div>
+        </form>
       </div>
     </div>
   )
